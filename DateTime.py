4013c6eb8a64c9aa9265c89d980c6684537534dd
Naufal002 <80965666+Naufal002@users.noreply.github.com>
2022-06-10 14:11:36 +0700

    New Update

--- DateTime.py
+++ DateTime.py
@@ -5,9 +5,9 @@ import calendar as kalender
 #DateTime
 hari_ini = dt.date.today()
 print("\n")
-print("Hari ini: ", hari_ini, "\n")
-print("="*20)
-print(f"hari ini adalah {hari_ini:%A}\n")
+print(hari_ini, "\n")
+print("="*22)
+print(f"hari ini adalah: {hari_ini:%A}\n")
 
 #Kalender
 year = 2021
@@ -15,11 +15,12 @@ month = 1
 print(kalender.month(year, month))
 
 #Tanggal Lahir
+print("="*22)
 tanggal = int(input("Tanggal: "))
 bulan = int(input("Bulan: ")) # Masukkan bulan menggunakan tipe data INT misalnya januari berarti bulan 01
 tahun = int(input("Tahun: "))
 
-print("="*20)
+print("="*22)
 
 tanggal_lahir = dt.date(tahun, bulan, tanggal)
 print(f"Tanggal lahir Anda: {tanggal_lahir}")
@@ -28,3 +29,8 @@ print(f"Anda lahir pada hari: {tanggal_lahir:%A}")
 umur_hari = hari_ini - tanggal_lahir
 umur_tahun = umur_hari.days // 365
 print(f"Umur anda adalah: {umur_tahun} tahun")
+print("="*22)
+
+Copyright = "="*3, " © Naufal Rizqi Ilham Gibran ", "="*3
+print(Copyright)
+print("\n")
